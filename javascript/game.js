87aded5ce63843f88fa4a3d4106957d54b0041f5
Waqar Habib <waqarhabib94@aggienetwork.com>
2019-03-12 13:17:14 -0500

    desgined basic css and pseudo coded JS file

---
+++ javascript/game.js
@@ -0,0 +1,35 @@
+// Grab reference to DOM elements
+
+
+
+
+// Create variables for game (wordBank, wins, losses, wordPicked, guessesLeft, gameRunning, picked word placeholder, guessed letter bank, incorrect letter bank)
+
+
+
+// newGame function to reset all stats, pick new word and create placeholders
+
+
+
+// letterGuess function: takes in the letter pressed and sees if it's a part of the selected word
+
+
+// checkIncorrect(letter)
+
+
+
+// checkLose
+
+
+
+// checkWin
+
+
+
+
+// EventLister for new game button
+
+
+
+
+// onekeyup event to trigger letterGuessed
